081c1cec50e7d7a116aa18b1eac5f881fcf0b98e
Danilo <daniloprates@gmail.com>
2019-11-18 14:07:28 -0300

    Version 1.2.0 (#1)
    * [V1.2.0] Init
    
    * [v1.2.0] New features

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+## 1.2.0
+
+### Features
+
+- Added text formatting and new argument options ([PR 1](https://github.com/daniloprates/branch-slug/pull/1))
--- index.js
+++ index.js
@@ -1,34 +1,60 @@
 #!/usr/bin/env node
+var program = require('commander');
 var slug = require('slug');
 var clipboardy = require('clipboardy');
+var chalk = require('chalk');
 
-var includeBranch = false;
-var doCopy = true;
-var hasType = true;
+program
+  .option('-g, --no-git', 'Don\'t include git command for new branch "git branch -m"')
+  .option('-c, --no-copy', 'Don\'t copy output to the clipboard')
+  .option('-t, --type <typet>', 'Branch type. The output is {type}/{description}');
+
+program.on('--help', function(){
+  console.log(`
+Types (-t {type}):
+  ${chalk.greenBright.bold('f')} or ${chalk.greenBright.bold('fe')} for ${chalk.blueBright('feature')}
+  ${chalk.greenBright.bold('fi')} for ${chalk.blueBright('fix')}
+  ${chalk.greenBright.bold('b')} for ${chalk.blueBright('bug')}
+  ${chalk.greenBright.bold('c')} for ${chalk.blueBright('chore')}
+  ${chalk.greenBright.bold('r')} for ${chalk.blueBright('release')}
+  or any other custom type e.g. ${chalk.greenBright.bold('special')}`)
+});
+
+program.parse(process.argv);
+
+var nextItemIsTypeDescription = false;
 
 var args = process.argv
   .slice(2)
-  .filter(function(value) {
-    if (value === '-b') {
-      includeBranch = true;
-    } else if (value === '-c') {
-      doCopy = false;
-    } else if (value === '-t') {
-      hasType = false;
-    } else {
-      return true;
+  .filter((item, index, array) => {
+    if (nextItemIsTypeDescription) {
+      nextItemIsTypeDescription = false;
+      return false;
     }
+    if (item === '-t' || item === '--type') {
+      nextItemIsTypeDescription = true
+    }
+    if (program.options.find(o => o.short === item || o.long === item)) {
+      return false
+    }
+    return true;
   });
 
-var branch = includeBranch ? 'git branch -m ' : '';
-var type = hasType ? `${args.splice(0, 1)}/` : '';
+var gitCommand = program.git ? 'git branch -m ' : '';
+var type = program.type || '';
+if (type === 'f' || type === 'fe') type = 'feature';
+else if (type === 'fi') type = 'fix';
+else if (type === 'b') type = 'bug';
+else if (type === 'c') type = 'chore';
+else if (type === 'r') type = 'release';
+if (type) type+='/'
 var description = slug(args.join('-'), { lower: true });
-
-var output = branch + type + description;
-
-if (doCopy) {
-  clipboardy.writeSync(output);
-  console.log('\n"%s" copied to clipboard\n', output);
-} else {
-  console.log('\n%s\n', output);
+var copyOutput = gitCommand + type + description;
+var printOutput = chalk.greenBright.bold(copyOutput);
+if (program.copy) {
+  clipboardy.writeSync(copyOutput);
+  printOutput += chalk.blueBright(' copied to clipboard.');
 }
+printOutput = chalk.blueBright.bold('Branch Slug: ') + printOutput;
+
+console.log('\n%s\n', printOutput);
